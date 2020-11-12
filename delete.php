f8dee70b2a96074b2a6e0083bc3616cc110a6a3c
Alexander <shushiks@gmail.com>
2020-11-12 18:24:18 +1000

    Update delete.php

--- delete.php
+++ delete.php
@@ -7,6 +7,7 @@
 <html lang="ru">
   <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="css/mycss.css">
     <title>Морозилочка</title>
   </head>
